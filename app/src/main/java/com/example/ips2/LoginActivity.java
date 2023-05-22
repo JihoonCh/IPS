33e52c54459c0b086e0eb224bebb54ba057cbad7
aroeypi <yeorane318@gmail.com>
2023-05-23 03:33:58 +0900

    with Splash

--- app/src/main/java/com/example/ips2/LoginActivity.java
+++ app/src/main/java/com/example/ips2/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.example.ips2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +7,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.ips.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/com/example/ips2/MainActivity.java
+++ app/src/main/java/com/example/ips2/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.ips2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.example.ips.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
